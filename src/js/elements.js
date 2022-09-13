86356b4411f37266e7015df168913f2bf8300e97
Tomek <92643157+TomaszJaniszta@users.noreply.github.com>
2022-09-13 21:10:58 +0200

    Update

--- src/js/elements.js
+++ src/js/elements.js
@@ -4,11 +4,17 @@ const overlay = document.getElementById('overlay');
 const resetBtn = document.getElementById('reset');
 const countBtn = document.getElementById('button');
 const spanCounter = document.getElementById('counter');
+const resetBall = document.getElementById('resetBall');
+const show1 = document.getElementById('show1');
+const show2 = document.getElementById('show2');
 
 xButton.addEventListener('click', () => {popupOnOff()});
 overlay.addEventListener('click', () => {popupOnOff()});
 resetBtn.addEventListener("click", () => {resetCounter()});
 countBtn.addEventListener('click', () => {counterButtonClick()});
+resetBall.addEventListener('click', () => {resetBallClick()});
+show1.addEventListener('click', () => {showHide('part1')});
+show2.addEventListener('click', () => {showHide('part2')});
 
 const imgMe = document.getElementById('imgMe');
 imgMe.addEventListener('click', () => {counterButtonClick()});
--- src/js/functionsAndVariables.js
+++ src/js/functionsAndVariables.js
@@ -1,5 +1,4 @@
 let counter = 0;
-
 function initFunction() {
 	counter = localStorage.getItem('count');
 	isNaN(counter) ? (spanCounter.innerText = 0, counter = 0) : spanCounter.innerText = counter;
@@ -23,4 +22,61 @@ const counterButtonClick = () => {
 	localStorage.setItem('count',counter);
 	if(counter > 5 && resetBtn.classList.length === 1){resetBtn.classList.toggle('r-active')};
 	popupOnOff();
+};
+
+let boardUpdate = false;
+
+const resetBallClick = () => {
+	//if(boardUpdate){
+		let divRow = 0;
+		let boardContainer = document.getElementById('boardContainer');
+	board.map((indexTable) => {
+		divRow++;
+		let boardRow = document.createElement("div");
+		boardRow.setAttribute("id", "divRow"+divRow);
+		boardContainer.appendChild(boardRow);
+		indexTable.map((element) => {
+		  if (element === 'X') {
+			let boardElement = document.createElement("span");
+				boardElement.innerHTML ='C';
+			let divContainer = document.getElementById("divRow"+divRow);
+				divContainer.appendChild(boardElement);
+			// return <
+				//	div className={styles.Wall}>C</div>; // g, C
+		  } else if (element === '1') {
+			let boardElement = document.createElement("span");
+				boardElement.innerHTML ='y';
+				boardElement.setAttribute("id", "ball");
+				// if(started){boardElement.setAttribute("class", "BallRunning")}else{boardElement.setAttribute("class", "Ball")}; // do zamiany
+				// <div id="ball" className={started ? styles.BallRunning : styles.Ball}>y</div> // orginał
+			let divContainer = document.getElementById("divRow"+divRow);
+				divContainer.appendChild(boardElement);
+		  } else if (element === '0') {
+			let boardElement = document.createElement("span");
+				boardElement.innerHTML ='c';
+			let divContainer = document.getElementById("divRow"+divRow);
+				divContainer.appendChild(boardElement);
+			// return <div className={styles.Air}>c</div>;
+		  } else if (element === 'Y') {
+			let boardElement = document.createElement("span");
+				boardElement.innerHTML ='s';
+			let divContainer = document.getElementById("divRow"+divRow);
+				divContainer.appendChild(boardElement);
+			// return <div className={styles.y}>s</div>;
+		  }
+		});
+		
+		boardContainer.appendChild(boardRow);
+		boardUpdate = !boardUpdate;
+	  //</div>
+	});
+};
+	
+  //} else {
+	// document.getElementById("board").remove(); 
+  //};
+  
+const showHide = (obj) => {
+    let x=document.getElementById(obj);
+    if(x.style.display == "none"){x.style.display = "block"} else {x.style.display = "none"};
 };
