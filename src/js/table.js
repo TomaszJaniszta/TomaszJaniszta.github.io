713c836c55483b62e20cf52892f9193e85d0e2eb
Tomek <92643157+TomaszJaniszta@users.noreply.github.com>
2022-10-22 22:27:40 +0200

    Update

--- src/js/table.js
+++ src/js/table.js
@@ -13,98 +13,97 @@ let sortAscDsc = true;
 let filterDescVal = '';
 let filterPriorVal = '';
 
-	function sortById(){
-		sortAscDsc = !sortAscDsc;
-		
-		function compr( obj1, obj2 ) {
-    		if ( obj1.work_order_id < obj2.work_order_id ){
-      			return -1;
-    		};
-    		if ( obj1.work_order_id > obj2.work_order_id ){
-      			return 1;
-    		};
-    		return 0;
-  		};
-		
-		if(sortAscDsc===true){
-			dataTable.sort(compr);
-		};
-		if(sortAscDsc===false){
-			dataTable.sort(compr).reverse();
-		};
-		//update view
-		loadData();
-	}
+function sortById(){
+	sortAscDsc = !sortAscDsc;
 	
-	function sortByDate(){
-		sortAscDsc = !sortAscDsc;
-		
-		function compr( obj1, obj2 ) {
-    		if ( obj1.received_date < obj2.received_date ){
-      			return -1;
-    		};
-    		if ( obj1.received_date > obj2.received_date ){
-      			return 1;
-    		};
-    		return 0;
-  		};
+	function compr( obj1, obj2 ) {
+	if ( obj1.work_order_id < obj2.work_order_id ){
+		return -1;
+	};
+	if ( obj1.work_order_id > obj2.work_order_id ){
+		return 1;
+	};
+	return 0;
+	};
+	
+	if(sortAscDsc===true){
+		dataTable.sort(compr);
+	};
+	if(sortAscDsc===false){
+		dataTable.sort(compr).reverse();
+	};
+	//update view
+	loadData();
+}
+	
+function sortByDate(){
+	sortAscDsc = !sortAscDsc;
+	
+	function compr( obj1, obj2 ) {
+	if ( obj1.received_date < obj2.received_date ){
+		return -1;
+	};
+	if ( obj1.received_date > obj2.received_date ){
+		return 1;
+	};
+	return 0;
+	};
 
-		if(sortAscDsc===false){
-			dataTable.sort(compr);
-		};
-		if(sortAscDsc===true){
-			dataTable.sort(compr).reverse();
-		};
-		loadData();
-	}
-	
-	function filterPriority(value){
-	    if(value !== 'All'){filterPriorVal = value} else {filterPriorVal = ''};
-		//main filter function
-		transformData(filterDescVal, filterPriorVal);
-		//update html info
-		updateDescForTable();
-		//update html
-		loadData();
+	if(sortAscDsc===false){
+		dataTable.sort(compr);
 	};
-	
-	function filterDesc(value){
-		filterDescVal = value;
-		transformData(filterDescVal, filterPriorVal);
-		updateDescForTable();
-		loadData();
+	if(sortAscDsc===true){
+		dataTable.sort(compr).reverse();
 	};
+	loadData();
+}
 	
-	function transformData(filterDescVal, filterPriorVal){
-		let tempData = JSON.parse(JSON.stringify(data.response.data));
-		if(filterDescVal == ''){
-			dataTable = tempData ;
-		}else{
-			//without white spaces
-			//console.log(filterDescVal);
-			//console.log(filterDescVal.toLowerCase().replace(/^\s+/, '').replace(/\s+$/, ''));
-	        tempData = tempData .filter((row) =>
-	        row.description.toLowerCase().replaceAll(/\s/g,'').includes(filterDescVal.toLowerCase().replaceAll(/\s/g,'')));
-			dataTable = tempData ;
-	    };
-		if(filterPriorVal){
-	        tempData = tempData .filter((row) =>
-	        row.priority.toLowerCase().includes(filterPriorVal.toLowerCase()));
-			dataTable = tempData ;
-	    };
-	};
+function filterPriority(value){
+	if(value !== 'All'){filterPriorVal = value} else {filterPriorVal = ''};
+	//main filter function
+	transformData(filterDescVal, filterPriorVal);
+	//update html info
+	updateDescForTable();
+	//update html
+	loadData();
+};
 	
-	function updateDescForTable(){
-		if(!filterDescVal && !filterPriorVal){
-			noTableData.innerHTML="";
-		} else {
-			noTableData.innerHTML=
-				(filterDescVal ? "Data filtered by decription containing: " + filterDescVal+". " : "") +
-				(filterPriorVal ? "Data filtered by priority: " + filterPriorVal+"." : "");
-		};
+function filterDesc(value){
+	filterDescVal = value;
+	transformData(filterDescVal, filterPriorVal);
+	updateDescForTable();
+	loadData();
+};
+	
+function transformData(filterDescVal, filterPriorVal){
+	let tempData = JSON.parse(JSON.stringify(data.response.data));
+	if(filterDescVal == ''){
+		dataTable = tempData ;
+	}else{
+		//without white spaces
+		//console.log(filterDescVal.toLowerCase().replace(/^\s+/, '').replace(/\s+$/, ''));
+	tempData = tempData .filter((row) =>
+	row.description.toLowerCase().replaceAll(/\s/g,'').includes(filterDescVal.toLowerCase().replaceAll(/\s/g,'')));
+		dataTable = tempData ;
 	};
+	if(filterPriorVal){
+	tempData = tempData .filter((row) =>
+	row.priority.toLowerCase().includes(filterPriorVal.toLowerCase()));
+		dataTable = tempData ;
+	};
+};
+	
+function updateDescForTable(){
+	if(!filterDescVal && !filterPriorVal){
+		noTableData.innerHTML="";
+	} else {
+		noTableData.innerHTML=
+			(filterDescVal ? "Data filtered by decription containing: " + filterDescVal+". " : "") +
+			(filterPriorVal ? "Data filtered by priority: " + filterPriorVal+"." : "");
+	};
+};
 		
-  const resetData = () => { 
+const resetData = () => { 
 	tableContainer.innerHTML = '';
 	tableFilters.setAttribute("style", "display: none");
 	dataTable = JSON.parse(JSON.stringify(data.response.data));
@@ -120,64 +119,58 @@ let filterPriorVal = '';
 	sortByDateBtn.setAttribute("style", "display: none");
 	document.getElementById('loadBtn').setAttribute("style", "display: visible");
 	document.getElementById('loadReset').setAttribute("style", "display: none");
-	
 	//document.getElementById('loadBtn').removetAttribute("style", "disabled");
-  };
+};
   
-  const loadData = () => {
+const loadData = () => {
 	document.getElementById('noTableData').innerHTML="";
 	tableFilters.setAttribute("style", "display: visible");
-	
-	let tableElement;
 	//let tableRow = 0;
 	let tableContainer = document.getElementById('tableContainer');
 	tableContainer.innerHTML = '';
-	
 	// headers
 		let headers = [];
 		let trTable = document.createElement("tr");
-		dataTable.forEach((element) => {
-			Object.entries(element).forEach((e) => {
+		dataTable.forEach((element) => {
+			Object.entries(element).forEach((e) => {
 				headers.push(e[0]);
 			});
 			//unique headers
 			headers = [...new Set(headers)];
 		});
 		
-		headers.forEach((h) => {
+		headers.forEach((h) => {
 			let header = document.createElement("th");
-			header.setAttribute("style", "border:1px solid; padding-inline:3px; background:gray;");
+			header.setAttribute("style","border:1px solid; padding-inline:3px; background:lightgray");
 			header.innerHTML = h;
 			trTable.appendChild(header);
 		});
 		//add headers
-		tableContainer.appendChild(trTable);
-				
-		
+		tableContainer.appendChild(trTable);	
 	// data rows
 		dataTable.forEach((indexTable) => {
 			//tableRow++;
 			let trTable = document.createElement("tr");
 			
-			Object.entries(indexTable).map((element) => {
+			Object.entries(indexTable).map((element) => {
 				// value parameter to take values without description
 				let value = 0
 				
-				element.forEach((e) => {
+				element.forEach((e) => {
 					if( value > 0 ){ 
-						let tableElement = document.createElement("td");
-							tableElement.setAttribute("style", "border: 1px solid; border-collapse:collapse; white-space:normal;");
+						let tableElement = document.createElement("td");
+							tableElement.setAttribute("style", "border: 1px solid");
 							
 						if(typeof(e) === 'object' && e.length > 0){
-							let tableManyElements = document.createElement("td");
-							tableManyElements.setAttribute("style", "display:flex; flex-direction:column; border-collapse:collapse;");
+							let tableManyElements = document.createElement("td");
+							tableManyElements.setAttribute("style", "display:flex; flex-direction:column; border-collapse:collapse; border-spacing:0; border: 1px solid;");
 							
-							e.forEach((x) => {
+							e.forEach((x) => {
 								//console.log(e);
 								//console.log(x);
-								let xManyElements = document.createElement("td");
-								xManyElements.setAttribute("style", "border: 1px solid; border-collapse:collapse;");
-								xManyElements.innerHTML = JSON.stringify(x).replace('{', '').replace('}', '').replace(',', ', ').replaceAll(':', ': ').replaceAll('"', '');
+								let xManyElements = document.createElement("td");
+								xManyElements.setAttribute("style", "border-collapse:collapse; border-spacing:0; border: none; border-block:1px solid black;");
+								xManyElements.innerHTML = JSON.stringify(x).replace('{', '').replace('}', '').replace(',', ', ').replaceAll(':', ': ').replaceAll('"', '');
 								tableManyElements.appendChild(xManyElements);
 							});
 
@@ -185,7 +178,7 @@ let filterPriorVal = '';
 							value--
 						} else {
 						//if(e == ""){ console.log("empty"); e = "empty";};
-						tableElement.innerHTML = e;
+						tableElement.innerHTML = e;
 						trTable.appendChild(tableElement);
 						value--;}
 					} else { value++ };
@@ -200,6 +193,6 @@ let filterPriorVal = '';
 	sortByDateBtn.setAttribute("style", "display: visible");
 	document.getElementById('loadBtn').setAttribute("style", "display: none");
 	document.getElementById('loadReset').setAttribute("style", "display: visible");
-  };
-  
-  
+};
+
+loadData();
